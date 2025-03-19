9106a6eed502d21800373aea9d439ef73a660a6d
zaen <gooner0709@gmail.com>
2025-03-20 06:16:14 +0700

    feat: ignore deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ podman manifest create gooner0709/gitolite:$1
 podman manifest add gooner0709/gitolite:$1 gooner0709/gitolite:amd64
 podman manifest add gooner0709/gitolite:$1 gooner0709/gitolite:arm64
 
-podman manifest push gooner0709/gitolite:$1 docker.io/gooner0709/gitolite:latest
 podman manifest push gooner0709/gitolite:$1 docker.io/gooner0709/gitolite:$1
+podman manifest push gooner0709/gitolite:$1 docker.io/gooner0709/gitolite:latest
